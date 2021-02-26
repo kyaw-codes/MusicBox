9147afe9d0a079873b94fa3de3ffcc80824a6e92
ko kyaw <kyaw.codes@gmail.com>
2021-02-26 11:54:57 +0630

    Initial commit

---
+++ MusicBox/View Controllers/HomeVC.swift
@@ -0,0 +1,17 @@
+//
+//  HomeVC.swift
+//  MusicBox
+//
+//  Created by Ko Kyaw on 26/02/2021.
+//
+
+import SwiftUI
+
+class HomeVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+}
